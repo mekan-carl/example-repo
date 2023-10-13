5017a0f5c7e5284b51407d6ec2453b3af5bd1105
Mekan Sahatvaliyev <smekan2320@gmail.com>
2023-10-13 21:52:09 +0300

    abc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Success from "./components/Success";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
